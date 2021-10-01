4b29aebdcb1a706105fef465368d0d5f40de07e3
Riya <hiyoriorria@gmail.com>
2021-10-01 22:28:10 +0900

    微細な変更

--- src/getArraysDiff.ts
+++ src/getArraysDiff.ts
@@ -13,7 +13,6 @@ const getArraysDiff = ((array: any[], ...arrays: any[]) => {
     for (const i of arrays) {
         hasArr.push(...i);
     }
-
     const arr01 = [...new Set(array)];
     const arr02 = [...new Set(hasArr)];
     return [...arr01, ...arr02].filter((value) => !arr01.includes(value) || !arr02.includes(value));
