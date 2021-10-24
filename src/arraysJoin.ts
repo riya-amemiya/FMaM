945db16ef54138aa0fbb2bd2c4f173eb1e7eabb1
Riya <hiyoriorria@gmail.com>
2021-10-25 01:04:12 +0900

    型推論の強化

--- src/arraysJoin.ts
+++ src/arraysJoin.ts
@@ -1,6 +1,7 @@
+export type ElemenType<X extends readonly any[] | any> = X extends readonly (infer A)[] ? A : never;
 export interface ARRAYSJOIN {
-    (): (array: any[], ...arrays: any[]) => unknown[];
-    (array: any[], ...arrays: any[]): unknown[];
+    (): <X extends unknown[], Y extends unknown[]>(array: X, ...arrays: Y) => (ElemenType<X> | ElemenType<ElemenType<Y>>)[];
+    <X extends unknown[], Y extends unknown[]>(array: X, ...arrays: Y): (ElemenType<X> | ElemenType<ElemenType<Y>>)[];
 }
 /**
  * 重複をしないで結合
